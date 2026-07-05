e7140327125a19533be3a807869a07bf69d0f84d
SilleryCa <DogWizardStudios@gmail.com>
2026-07-05 17:22:50 +0100

    Animate player on fly

--- scenes/tappy/Tappy.gd
+++ scenes/tappy/Tappy.gd
@@ -5,6 +5,9 @@ const JUMP_FORCE: float = -350.0
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 var _jumped: bool = false
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("fly"):
 		_jumped = true
@@ -15,6 +18,8 @@ func _physics_process(delta: float) -> void:
 	
 	if _jumped:
 		velocity.y = JUMP_FORCE
+		animation_player.play("fly")
 		_jumped = false
+		
 	
 	move_and_slide()
